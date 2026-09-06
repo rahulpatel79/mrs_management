78af81b86cd3704f774b3fc485e65bf20f3e0169
Raja Patel <rajagurjer55@gmail.com>
2026-09-07 01:31:50 +0530

    add graph chard new design

--- src/pages/Dashboard/components/DashboardBox.js
+++ src/pages/Dashboard/components/DashboardBox.js
@@ -1,18 +1,99 @@
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import Button from "@mui/material/Button";
+import { HiDotsVertical } from "react-icons/hi";
+import { IoMdTimer } from "react-icons/io";
+
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from 'react';
 
 const DashboardBox = (props) => {
-
- 
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const ITEM_HEIGHT = 48;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-      <div className="dashboardBox" style={{
-        backgroundImage: `linear-gradient(to right, ${props.color?.[0]}, ${props.color?.[1]})`
-         
-      }}></div>
-               
-               
+      <div
+        className="dashboardBox"
+        style={{
+          backgroundImage: `linear-gradient(to right, ${props.color?.[0]}, ${props.color?.[1]})`,
+        }}
+      >
+        {props.grow === true ? (
+          <span className="chart">
+            {" "}
+            <TrendingUpIcon />{" "}
+          </span>
+        ) : (
+          <span className="chart">
+            {" "}
+            <TrendingDownIcon />{" "}
+          </span>
+        )}
+
+        <div className="d-flex w-100">
+          <div className="col1">
+            <h4 className="text-white mb-0">Total User</h4>
+            <span className="text-white">277</span>
+          </div>
+          <div className="ms-auto">
+            {props.icon ? (
+              <span className="icon">{props.icon ? props.icon : ""}</span>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+
+        <div className="d-flex align-items-center w-100 bottomEle">
+          <h6 className="text-white mb-0 mt-0">Last Month</h6>
+
+          <div className="ms-auto">
+            <Button className="ms-auto toggleIcon"  onClick={handleClick}>
+              <HiDotsVertical />
+              
+
+              
+            </Button>
+            <Menu className='dropdown_manu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          paper: {
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '20ch',
+            },
+          },
+          list: {
+            'aria-labelledby': 'long-button',
+          },
+        }}
+      >
+          
+          <MenuItem  onClick={handleClose}>
+             
+            <IoMdTimer /> Last Day
+          </MenuItem>
+          <MenuItem  onClick={handleClose}>
+             
+            <IoMdTimer /> Last Week
+          </MenuItem>
+        
+      
+              </Menu>
+          </div>
+        </div>
+      </div>
     </>
-    
-  )
-}
- export default DashboardBox
+  );
+};
+export default DashboardBox;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,26 +1,176 @@
+import Button from "@mui/material/Button";
+import { FaCircleUser } from "react-icons/fa6";
+import { HiShoppingCart } from "react-icons/hi";
+import { IoMdTimer } from "react-icons/io";
+import { IoBagHandle, IoStarHalf } from "react-icons/io5";
+
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from 'react';
 import DashboardBox from './components/DashboardBox';
 
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2004", 1000, 400],
+  ["2005", 1170, 460],
+  ["2006", 660, 1120],
+  ["2007", 1030, 540],
+];
+
+export const options = {
+  title: "Sales & Expenses",
+  curveType: "function",
+
+  backgroundColor: "transparent",
+
+  chartArea: {
+    left: 55,
+    top: 45,
+    width: "86%",
+    height: "68%",
+    backgroundColor: "transparent",
+  },
+
+  colors: [
+    "#63F5FF", // Sales - cyan
+    "#FFD166", // Expenses - gold
+  ],
+
+  legend: {
+    position: "bottom",
+    textStyle: {
+      color: "#FFFFFF",
+      fontSize: 12,
+    },
+  },
+
+  titleTextStyle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    bold: true,
+  },
+
+  hAxis: {
+    textStyle: {
+      color: "rgba(255,255,255,0.75)",
+      fontSize: 11,
+    },
+    gridlines: {
+      color: "transparent",
+    },
+    baselineColor: "rgba(255,255,255,0.25)",
+  },
+
+  vAxis: {
+    textStyle: {
+      color: "rgba(255,255,255,0.75)",
+      fontSize: 11,
+    },
+    gridlines: {
+      color: "rgba(255,255,255,0.10)",
+      count: 5,
+    },
+    baselineColor: "transparent",
+  },
+
+  pointSize: 6,
+
+  lineWidth: 3,
+
+  focusTarget: "category",
+
+  tooltip: {
+    textStyle: {
+      color: "#172554",
+      fontSize: 13,
+    },
+    showColorCode: true,
+  },
+
+  animation: {
+    startup: true,
+    duration: 1200,
+    easing: "out",
+  },
+};
+
 function Dashboard() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const ITEM_HEIGHT = 48;
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
   return (
     <>
       <div className="right-content w-100">
         <div className="row dashboardBoxWrapperRow">
-          <div className="col-md-8 ">
+          <div className="col-md-8">
             <div className="dashboardBoxWrapper d-flex">
-              <DashboardBox color={["#d7d7d1","#009c05"]} />
-              <DashboardBox color={["#b5c3ca","#bc3030"]} />
-              <DashboardBox color={["#6a1d50","#9d7070"]} />
-              <DashboardBox color={["#169172","#12166b78"]} />
+              <DashboardBox color={["#1da256", "#48d483"]} icon={<FaCircleUser />} grow={true} />
+              <DashboardBox color={["#c012e2", "#eb64fe"]} icon={<HiShoppingCart />} grow={true} />
+              <DashboardBox color={["#2c78e5", "#60aff5"]} icon={<IoBagHandle />} grow={true}/>
+              <DashboardBox color={["#e1950e", "#f3cd29"]} icon={<IoStarHalf /> } grow={true}/>
                
             </div>
           </div>
 
           <div className="col-md-4 pl-0">
-            <div className="box">
+            <div className="box graphBox">
+                   <div className="d-flex align-items-center w-100 bottomEle top-0">
+          <h6 className="text-white mb-0 mt-0">Last Month</h6>
+
+          <div className="ms-auto">
+            <Button className="ms-auto toggleIcon"  onClick={handleClick}>
+              <MoreHorizIcon />
               
-      </div>
+
+              
+            </Button>
+            <Menu className='dropdown_manu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          paper: {
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '20ch',
+            },
+          },
+          list: {
+            'aria-labelledby': 'long-button',
+          },
+        }}
+      >
+          
+          <MenuItem  onClick={handleClose}>
+             
+            <IoMdTimer /> Last Day
+          </MenuItem>
+          <MenuItem  onClick={handleClose}>
+             
+            <IoMdTimer /> Last Week
+          </MenuItem>
+        
+      
+              </Menu>
+          </div>
+              </div>
+
+                  <h3 className="text-white fw-bold">$3,787,681.00</h3>
+                  <p>$3,787,681.00 in last month</p>
+                   <Chart className='chartDesign' chartType="LineChart" width="100%" height="290px" data={data} options={options}/>
+            </div>
           </div>
 
+        
         </div>
       </div>
     </>
